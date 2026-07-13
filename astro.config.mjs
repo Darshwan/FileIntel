728e0099ef236988fb494f750580d72903eafb76
Darshan Lamichhane <lamichhane3848@gmail.com>
2026-07-13 19:07:40 +0545

    Add sitemap integration and update dependencies in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://fileintel.me',
+  integrations: [
+    tailwind(),
+    sitemap(),
+  ],
   server: {
     host: true,
   },
