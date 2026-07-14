69a8cdb00bd7586c919f17a7a8ec43d8db42c8b6
Darshan Lamichhane <lamichhane3848@gmail.com>
2026-07-14 07:47:55 +0545

    Add sitemap link to footer and adjust sidebar positioning across multiple pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.fileintel.me',
+  site: 'https://fileintel.me',
   integrations: [
     tailwind(),
     sitemap(),
